4899f969e75df4fefc00105011ee6f247f8e8d40
tanqhr <tanqhr@abv.bg>
2026-05-14 16:52:15 +0300

    Adding scroller

--- altea/script.js
+++ altea/script.js
@@ -1,12 +1,34 @@
-const mobile = window.matchMedia("(max-width: 768px)");
-const img = document.getElementById("center");
+// const mobile = window.matchMedia("(max-width: 768px)");
+// const img = document.getElementById("center");
 
-function handleScreen(e) {
-  img.src = e.matches
-    ? "./images/2zEYhy.jpeg"
-    : "./images/ecxI3y.jpeg";
-}
+// function handleScreen(e) {
+//   img.src = e.matches
+//     ? "./images/2zEYhy.jpeg"
+//     : "./images/ecxI3y.jpeg";
+// }
 
-handleScreen(mobile);
+// handleScreen(mobile);
 
-mobile.addEventListener("change", handleScreen);
+// mobile.addEventListener("change", handleScreen);
+
+const scrollBtn = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        scrollBtn.style.display = "flex";
+        scrollBtn.style.justifyContent = "center";
+        scrollBtn.style.alignItems = "center";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
